8adfbfd0dff80036be95150c11e8de44894251d4
Domingas Coelho <domingas1903@hotmail.com>
2022-09-25 16:25:04 +0300

    django autentication

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -136,3 +136,16 @@ STATICFILES_DIRS = [
 #media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+#SMTP CONFIGURATION
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'domingasth97@gmail.com'
+EMAIL_HOST_PASSWORD = 'avva rbuf jivw laom'
+EMAIL_USE_TLS = True 
